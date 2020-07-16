49225aadf128a31e5a88f799d53cac0b58588ed7
shiva prasad <shiva@secondsdigital.com>
2020-07-16 15:40:30 +0530

    Second commit

---
+++ assets/js/light.js
@@ -0,0 +1,187 @@
+AmCharts.themes.light = {
+
+	AmChart: {
+		color: "#540000", backgroundColor: "#FFFFFF"
+	},
+
+	AmCoordinateChart: {
+		colors: ["#d8c26b", "#540000", "#ea4c89", "#540000", "#cd82ad", "#2f4074", "#448e4d", "#b7b83f", "#d8c26b", "#540000", "#913167"]
+	},
+
+	AmStockChart: {
+		colors: ["#d8c26b", "#540000", "#ea4c89", "#540000", "#cd82ad", "#2f4074", "#448e4d", "#b7b83f", "#d8c26b", "#540000", "#913167"]
+	},
+
+	AmSlicedChart: {
+		colors: ["#d8c26b", "#540000", "#ea4c89", "#540000", "#cd82ad", "#2f4074", "#448e4d", "#b7b83f", "#d8c26b", "#540000", "#913167"],
+		outlineAlpha: 1,
+		outlineThickness: 2,
+		labelTickColor: "#540000",
+		labelTickAlpha: 0.3
+	},
+
+	AmRectangularChart: {
+		zoomOutButtonColor: '#540000',
+		zoomOutButtonRollOverAlpha: 0.15,
+		zoomOutButtonImage: "lens"
+	},
+
+	AxisBase: {
+		axisColor: "#540000",
+		axisAlpha: 0.3,
+		gridAlpha: 0.1,
+		gridColor: "#540000"
+	},
+
+	ChartScrollbar: {
+		backgroundColor: "#540000",
+		backgroundAlpha: 0.12,
+		graphFillAlpha: 0.5,
+		graphLineAlpha: 0,
+		selectedBackgroundColor: "#FFFFFF",
+		selectedBackgroundAlpha: 0.4,
+		gridAlpha: 0.15
+	},
+
+	ChartCursor: {
+		cursorColor: "#540000",
+		color: "#FFFFFF",
+		cursorAlpha: 0.5
+	},
+
+	AmLegend: {
+		color: "#540000"
+	},
+
+	AmGraph: {
+		lineAlpha: 0.9
+	},
+	GaugeArrow: {
+		color: "#333",
+		alpha: 0.8,
+		nailAlpha: 0,
+		innerRadius: "40%",
+		nailRadius: 15,
+		startWidth: 15,
+		borderAlpha: 0.8,
+		nailBorderAlpha: 0
+	},
+
+	GaugeAxis: {
+		tickColor: "#333",
+		tickAlpha: 1,
+		tickLength: 15,
+		minorTickLength: 8,
+		axisThickness: 3,
+		axisColor: '#540000',
+		axisAlpha: 1,
+		bandAlpha: 0.8
+	},
+
+	TrendLine: {
+		lineColor: "#c03246",
+		lineAlpha: 0.8
+	},
+
+	// ammap
+	AreasSettings: {
+		alpha: 0.8,
+		color: "#999",
+		colorSolid: "#003767",
+		unlistedAreasAlpha: 0.4,
+		unlistedAreasColor: "#540000",
+		outlineColor: "#FFFFFF",
+		outlineAlpha: 0.5,
+		outlineThickness: 0.5,
+		rollOverColor: "#3c5bdc",
+		rollOverOutlineColor: "#FFFFFF",
+		selectedOutlineColor: "#FFFFFF",
+		selectedColor: "#f15135",
+		unlistedAreasOutlineColor: "#FFFFFF",
+		unlistedAreasOutlineAlpha: 0.5
+	},
+
+	LinesSettings: {
+		color: "#540000",
+		alpha: 0.8
+	},
+
+	ImagesSettings: {
+		alpha: 0.8,
+		labelColor: "#540000",
+		color: "#540000",
+		labelRollOverColor: "#3c5bdc"
+	},
+
+	ZoomControl: {
+		buttonFillAlpha:0.7,
+		buttonIconColor:"#a7a7a7"
+	},
+
+	SmallMap: {
+		mapColor: "#540000",
+		rectangleColor: "#f15135",
+		backgroundColor: "#FFFFFF",
+		backgroundAlpha: 0.7,
+		borderThickness: 1,
+		borderAlpha: 0.8
+	},
+
+	// the defaults below are set using CSS syntax, you can use any existing css property
+	// if you don't use Stock chart, you can delete lines below
+	PeriodSelector: {
+		color: "#540000"
+	},
+
+	PeriodButton: {
+		color: "#540000",
+		background: "transparent",
+		opacity: 0.7,
+		border: "1px solid rgba(0, 0, 0, .3)",
+		MozBorderRadius: "5px",
+		borderRadius: "5px",
+		margin: "1px",
+		outline: "none",
+		boxSizing: "border-box"
+	},
+
+	PeriodButtonSelected: {
+		color: "#540000",
+		backgroundColor: "#b9cdf5",
+		border: "1px solid rgba(0, 0, 0, .3)",
+		MozBorderRadius: "5px",
+		borderRadius: "5px",
+		margin: "1px",
+		outline: "none",
+		opacity: 1,
+		boxSizing: "border-box"
+	},
+
+	PeriodInputField: {
+		color: "#999",
+		background: "transparent",
+		border: "1px solid rgba(0, 0, 0, .3)",
+		outline: "none"
+	},
+
+	DataSetSelector: {
+
+		color: "#540000",
+		selectedBackgroundColor: "#b9cdf5",
+		rollOverBackgroundColor: "#a8b0e4"
+	},
+
+	DataSetCompareList: {
+		color: "#540000",
+		lineHeight: "100%",
+		boxSizing: "initial",
+		webkitBoxSizing: "initial",
+		border: "1px solid rgba(0, 0, 0, .3)"
+	},
+
+	DataSetSelect: {
+		border: "1px solid rgba(0, 0, 0, .3)",
+		outline: "none"
+	}
+
+};
