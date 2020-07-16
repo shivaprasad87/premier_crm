a7ffd1b268eee66300be647c7403fe2cfd9c8b92
shiva prasad <shiva@secondsdigital.com>
2020-07-17 00:26:11 +0530

    noindex and robots

--- application/views/inc/admin_header.php
+++ application/views/inc/admin_header.php
@@ -4,7 +4,7 @@
 <head><meta http-equiv="Content-Type" content="text/html; charset=utf-8">
 <title>Admin</title>
 <meta name="viewport" content="width=device-width, initial-scale=1">
-
+<meta name="robots" content="noindex">
 <meta name="keywords" content="Augment Responsive web template, Bootstrap Web Templates, Flat Web Templates, Android Compatible web template, 
 Smartphone Compatible web template, free webdesigns for Nokia, Samsung, LG, SonyEricsson, Motorola web design" />
 <script type="application/x-javascript"> addEventListener("load", function() { setTimeout(hideURLbar, 0); }, false); function hideURLbar(){ window.scrollTo(0,1); } </script>
